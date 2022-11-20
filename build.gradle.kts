a0e6fb805d4ccb5e29098152b4cae1277f8ece43
Nick Hensel <nickhensel25@icloud.com>
2022-11-20 21:16:34 +0100

    feat(gradle): Add install git hook task

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,4 +13,13 @@ dependencies {
 tasks {
     register<tequila.ValidateCommitsGitTask>("validateCommits")
     register<tequila.ValidateCommitMessageGitTask>("validateCommitMessageGit")
+    register<tequila.InstallGitHook>("installGitHook")
+
+    register("setup") {
+        dependsOn("installGitHook")
+    }
+
+    assemble {
+        dependsOn("setup") // auto install git hook when project is built
+    }
 }
